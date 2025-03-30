015bb84775c69e6ca507b9451b8f226b01e807f6
Yash Rawalkar <yashrawalkar04@gmail.com>
2025-03-30 19:12:01 +0530

    Pattern-4 N- DImensional triangle

---
+++ Striver/Step-1/Lec-2/JavaScript/Pattern-4.js
@@ -0,0 +1,18 @@
+"use strict";
+// N-dimensional triangle where each row contains the row number
+function halfForestWithSameNumber(num1) {
+    if (num1 < 1 || num1 > 25) {
+        console.error("Number must be between 1 and 25");
+        return;
+    }
+    for (let i = 1; i <= num1; i++) {
+        const row = Array(i).fill(i) // Creates an array with the length i, filled with the row number i
+            .join(" "); // Converts the array into the space separated string
+        console.log(row);
+    }
+}
+// Preferred way of printing the result
+const number1 = 10;
+halfForestWithSameNumber(number1);
+// Prints the same result but throws an undefined at the end
+console.log(halfForestWithSameNumber(6));
